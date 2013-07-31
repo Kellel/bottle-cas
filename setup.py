919fee8f75eccbe7e037ddafc79dd5d671f14adb
Kellen Fox <kellen@kellen.pwnz.org>
2013-07-31 12:39:41 -0700

    Increment the version number
    the version number should change for the OOP implementation.
    Also this version implements logout redirection.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='bottle-cas',
-      version='1.0',
+      version='1.1',
       description='CAS Client written in python for the bottle framework',
       author='Kellen Fox',
       url='http://github.com/Kellel/bottle-cas',
