3279942019fd41b8592d607d191555dcd3366b98
Kellen Fox <Kellen.Fox@wwu.edu>
2014-09-09 10:47:27 -0700

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='bottle-cas',
-      version='1.2.1',
+      version='1.2.1a',
       description='CAS Client written in python for the bottle framework',
       author='Kellen Fox',
       url='http://github.com/Kellel/bottle-cas',
