d32520be75e5cab56323109ecf6e0e65a1993390
Kyle Daling <dalingk@wwu.edu>
2017-02-27 16:02:55 -0800

    Provided CRON example for cleanup.sh

--- examples/cleanup.sh
+++ examples/cleanup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Shell script to clean up defualt beaker directory
+# Copy this script to /root and install in CRON with the quoted string on the
+# next line
+# "0 2 * * * /root/cleanup.sh &> /dev/null"
+
 BEAKER_DATA_DIR=/tmp/beaker/data
 BEAKER_LOCK_DIR=/tmp/beaker/lock
 
